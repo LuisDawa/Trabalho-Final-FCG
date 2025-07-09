275b4983c6bb468eb18d345ae2f8e7291d3b4476
jeanargoud <jean.s.argoud@gmail.com>
2025-07-09 00:23:34 -0300

    texturas na iluminação de gouraud

--- src/shader_fragment_gouraud.glsl
+++ src/shader_fragment_gouraud.glsl
@@ -8,13 +8,48 @@ out vec4 color;
 // Variável para o preview
 uniform float u_alpha = 1.0; // Valor padrão é 1.0 (opaco)
 
+// Coordenadas de textura obtidas do arquivo OBJ (se existirem!)
+in vec2 texcoords;
+
 // Constantes
 #define M_PI   3.14159265358979323846
 #define M_PI_2 1.57079632679489661923
 
+uniform sampler2D FloorTexture;
+uniform sampler2D SkyboxTexture;
+uniform sampler2D WoodTexture;
+uniform sampler2D ConcreteTexture;
+uniform sampler2D RubberTexture;
+uniform sampler2D FireTexture;
+
+uniform int object_id;
+#define SKY  0
+#define ROCKS  1
+#define WOOD  2
+#define CONCRETE  3
+#define RUBBER  4
+#define FIRE  5
+
 void main()
 {
-    vec3 gamma_corrected = pow(vertex_color.rgb, vec3(1.0 / 2.2));
-    color = vec4(gamma_corrected, vertex_color.a * u_alpha);
+    vec3 texColor = vec3(1.0);
+
+    if (object_id == ROCKS)
+        texColor = texture(FloorTexture, texcoords).rgb;
+    else if (object_id == WOOD)
+        texColor = texture(WoodTexture, texcoords).rgb;
+    else if (object_id == CONCRETE)
+        texColor = texture(ConcreteTexture, texcoords).rgb;
+    else if (object_id == RUBBER)
+        texColor = texture(RubberTexture, texcoords).rgb;
+    else if (object_id == FIRE)
+        texColor = texture(FireTexture, texcoords).rgb;
+    else if (object_id == SKY)
+        texColor = texture(SkyboxTexture, texcoords).rgb;
+
+    vec3 finalColor = texColor * vertex_color.rgb * 2.0; // leve aumento
+    finalColor = pow(finalColor, vec3(1.0 / 2.2));        // correção gama
+
+    color = vec4(finalColor, 1.0);
 } 
 
